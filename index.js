38b051e02fe7d0b684bf3c390a47fbf84feb2eba
Bob8552 <bobfromdpa@gmail.com>
2020-09-16 16:56:46 +0000

    Publish site: Added GridIt

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+//Database
+const Database = require("@replit/database");
+const db = new Database();
 
 app.use(express.static("public"));
 
