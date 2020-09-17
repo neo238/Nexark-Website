8c3cc0039bb1732e490923370cbfa7c71465a4b0
Bob8552 <bobfromdpa@gmail.com>
2020-09-17 16:48:42 +0000

    Publish site: New chat system!

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+var http = require('http').Server(app);
 const port = process.env.PORT;
+var io = require('socket.io')(http);
+
+const users = {}
+
 //Database
 const Database = require("@replit/database");
 const db = new Database();
@@ -11,6 +16,18 @@ app.get('/', (req, res) => {
 	res.status(200).sendFile(__dirname + "/views/index.html");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+
+http.listen(port);
--- public/app.js
+++ public/app.js
@@ -2,5 +2,46 @@
 
 //On Load
 window.addEventListener("load", () => {
-	console.log("--Ready & Loaded!--")
+	console.log("--Ready & Loaded!--");
 });
+
+//Start up the chat.
+function startChat() {
+	setTimeout(() => {
+		const socket = io('https://nexark.bob8552.repl.co/');
+		const messageContainer = document.getElementById('message-container');
+		const messageForm = document.getElementById('send-container');
+		const messageInput = document.getElementById('message-input');
+
+		const name = prompt('Please enter your desired username');
+		appendMessage('You joined');
+		socket.emit('new-user', name);
+
+		socket.on('chat-message', data => {
+			appendMessage(`[${data.name}]: ${data.message}`);
+		});
+
+		socket.on('user-connected', name => {
+			appendMessage(`[${name || "Unknown User"}] connected, say hi!`);
+		});
+
+		socket.on('user-disconnected', name => {
+			appendMessage(`[${name || "Unknown User"}] has disconnected.`);
+		});
+
+		messageForm.addEventListener('submit', e => {
+			e.preventDefault();
+			const message = messageInput.value;
+			if (message === "" || message === " ") return appendMessage("[SYSTEM]: You cannot send an empty message!");
+			appendMessage(`[${name} (You)]: ${message}`);
+			socket.emit('send-chat-message', message);
+			messageInput.value = '';
+		});
+
+		function appendMessage(message) {
+			const messageElement = document.createElement('div');
+			messageElement.innerText = message;
+			messageContainer.append(messageElement);
+		}
+	}, 50);
+};
