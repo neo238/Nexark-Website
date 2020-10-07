7d78d134556564709c86e747a7dbb1418d9e3946
Bob8552 <bobfromdpa@gmail.com>
2020-10-07 15:33:20 +0000

    Publish Site: Changed group descriptions

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const app = express();
-var http = require('http').Server(app);
 const port = process.env.PORT;
-var io = require('socket.io')(http);
-
-const users = {}
-
-//Database
-const Database = require("@replit/database");
-const db = new Database();
 
 app.use(express.static("public"));
 
@@ -16,18 +8,4 @@ app.get('/', (req, res) => {
 	res.status(200).sendFile(__dirname + "/views/index.html");
 });
 
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
-
-http.listen(port);
+app.listen(port);
